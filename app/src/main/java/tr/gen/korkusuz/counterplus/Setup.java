5f7ee2ba23a5f862bc4162b6fc42143a0bf05ce5
qpulsar <korkusuz@balikesir.edu.tr>
2019-10-22 11:42:40 +0300

    Setup activity eklendi

--- app/src/main/java/tr/gen/korkusuz/counterplus/Setup.java
+++ app/src/main/java/tr/gen/korkusuz/counterplus/Setup.java
@@ -1,4 +1,113 @@
 package tr.gen.korkusuz.counterplus;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class Setup {
+
+    int maxLimit = 10;
+    int minLimit = 0;
+    int currentCount = 0;
+
+    boolean upperVibration = true;
+    boolean upperPlaySound = true;
+    boolean lowerVibration = true;
+    boolean lowerPlaySound = true;
+
+
+    static Setup setup = null;
+
+    Context context;
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    private Setup(Context context){
+        this.context = context;
+        sharedPreferences = context.getSharedPreferences("Setup",0);
+        editor = sharedPreferences.edit();
+    }
+
+    public static Setup getInstance(Context context){
+        if (setup == null){
+            setup = new Setup(context);
+        }
+        return  setup;
+    }
+
+    public void saveValues(){
+        editor.putInt("currentCount", getCurrentCount());
+        editor.putInt("maxLimit",getMaxLimit());
+        editor.putInt("minLimit",getMinLimit());
+        editor.putBoolean("upperVibration",isUpperVibration());
+        editor.putBoolean("upperPlaySound",isUpperPlaySound());
+        editor.putBoolean("lowerVibration",isLowerVibration());
+        editor.putBoolean("lowerPlaySound",isLowerPlaySound());
+        editor.commit();
+    }
+    public void readValues(){
+        setCurrentCount(sharedPreferences.getInt("currentCount",0));
+        setMaxLimit(sharedPreferences.getInt("maxLimit",0));
+        setMinLimit(sharedPreferences.getInt("minLimit",0));
+        setUpperVibration(sharedPreferences.getBoolean("upperVibration",true));
+        setUpperPlaySound(sharedPreferences.getBoolean("upperPlaySound",true));
+        setLowerVibration(sharedPreferences.getBoolean("lowerVibration",true));
+        setLowerPlaySound(sharedPreferences.getBoolean("lowerPlaySound",true));
+    }
+
+    public int getMaxLimit() {
+        return maxLimit;
+    }
+
+    public void setMaxLimit(int maxLimit) {
+        this.maxLimit = maxLimit;
+    }
+
+    public int getMinLimit() {
+        return minLimit;
+    }
+
+    public void setMinLimit(int minLimit) {
+        this.minLimit = minLimit;
+    }
+
+    public int getCurrentCount() {
+        return currentCount;
+    }
+
+    public void setCurrentCount(int currentCount) {
+        this.currentCount = currentCount;
+    }
+
+    public boolean isUpperVibration() {
+        return upperVibration;
+    }
+
+    public void setUpperVibration(boolean upperVibration) {
+        this.upperVibration = upperVibration;
+    }
+
+    public boolean isUpperPlaySound() {
+        return upperPlaySound;
+    }
+
+    public void setUpperPlaySound(boolean upperPlaySound) {
+        this.upperPlaySound = upperPlaySound;
+    }
+
+    public boolean isLowerVibration() {
+        return lowerVibration;
+    }
+
+    public void setLowerVibration(boolean lowerVibration) {
+        this.lowerVibration = lowerVibration;
+    }
+
+    public boolean isLowerPlaySound() {
+        return lowerPlaySound;
+    }
+
+    public void setLowerPlaySound(boolean lowerPlaySound) {
+        this.lowerPlaySound = lowerPlaySound;
+    }
 }
--- app/src/main/java/tr/gen/korkusuz/counterplus/SetupActivity.java
+++ app/src/main/java/tr/gen/korkusuz/counterplus/SetupActivity.java
@@ -3,12 +3,32 @@ package tr.gen.korkusuz.counterplus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class SetupActivity extends AppCompatActivity {
+
+    Setup setup = null;
+
+    public static String TAG = "SETUPACTIVITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+        setup= Setup.getInstance(getApplicationContext());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setup.readValues();
+        Log.d(TAG, "onResume: read values");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: save values");
+        setup.saveValues();
     }
 }
